710578153780649cbcd24e874920cd0edc5784a4
Przemyslaw Wozniak <inz.przemyslaw.wozniak@gmail.com>
2020-06-07 14:00:22 +0200

    Completed Exercise 4

--- src/main/java/pl/sda/clockex/Clock.java
+++ src/main/java/pl/sda/clockex/Clock.java
@@ -24,17 +24,51 @@ public class Clock {
 
         if(hour > 23)
             this.hour = 23;
+        if(hour < 0)
+            this.hour = 0;
     }
 
     public void setMinutes(int minutes) {
         this.minutes = minutes;
+
+        if(minutes > 59)
+            this.minutes = 59;
+        if(minutes < 0)
+            this.minutes = 0;
     }
 
+    //1:30
+    //+30min
+    //60min
     public void addMinutes(int minutes) {
-        this.minutes += minutes;
+        int summedMinutes = this.minutes + minutes;
+        if(summedMinutes > 59) {
+            int hoursFromMinutes = summedMinutes/60;    //61/60 = 1; 30/60 = 0
+            addHours(hoursFromMinutes);
+
+            this.minutes = summedMinutes - hoursFromMinutes * 60;
+        }
+        else {
+            this.minutes += minutes;
+        }
+    }
+
+    //23:00
+    //+2h
+    //25h % 24 = 1
+    //-> 1:00
+    public void addHours(int hours) {
+        int summedHours = this.hour + hours;
+        if(summedHours > 23) {
+            this.hour = summedHours % 24;
+        }
+        else {
+            this.hour += hours;
+        }
     }
 
     public void addClock(Clock another) {
-
+        if(another != null)
+            this.addMinutes(another.getHour() * 60 + another.getMinutes());
     }
 }
--- src/test/java/pl/sda/clockex/ClockTest.java
+++ src/test/java/pl/sda/clockex/ClockTest.java
@@ -23,7 +23,13 @@ public class ClockTest {
 
     @Test
     public void create_and_set_initial_time() {
+        //given
+        c = new Clock(10, 30);
+        //when
 
+        //then
+        assertEquals(30, c.getMinutes());
+        assertEquals(10, c.getHour());
     }
     //--- create
 
@@ -40,17 +46,30 @@ public class ClockTest {
 
     @Test
     public void set_hour_to_max_value() {
+        //given
 
+        //when
+        c.setHour(23);
+        //then
+        assertEquals(23, c.getHour());
     }
 
     @Test
     public void set_minutes_to_min_value() {
+        //given
 
+        //when
+        c.setMinutes(0);
+        //then
+        assertEquals(0, c.getMinutes());
     }
 
     @Test
     public void set_minutes_to_max_value() {
-
+        //when
+        c.setMinutes(59);
+        //then
+        assertEquals(59, c.getMinutes());
     }
     //--- in range
     //--- out of range
@@ -66,17 +85,26 @@ public class ClockTest {
 
     @Test
     public void set_hour_below_range() {
-
+        //when
+        c.setHour(-1);
+        //then
+        assertEquals(0, c.getHour());
     }
 
     @Test
     public void set_minutes_above_range() {
-
+        //when
+        c.setMinutes(60);
+        //then
+        assertEquals(59, c.getMinutes());
     }
 
     @Test
     public void set_minutes_below_range() {
-
+        //when
+        c.setMinutes(-1);
+        //then
+        assertEquals(0, c.getMinutes());
     }
     //--- out of range
 
@@ -88,33 +116,57 @@ public class ClockTest {
         //when
         c.addMinutes(29);
         //then
-        assertEquals(59, c.getMinutes());
         assertEquals(1, c.getHour());
+        assertEquals(59, c.getMinutes());
     }
 
     @Test
     public void add_minutes_01_30_plus_31min_gives_02_01() {
-
+        //given
+        c = new Clock(1, 30);
+        //when
+        c.addMinutes(31);
+        //then
+        assertEquals(2, c.getHour());
+        assertEquals(1, c.getMinutes());
     }
 
     @Test
     public void add_minutes_01_30_plus_30min_gives_02_00() {
-
+        //given
+        c = new Clock(1, 30);
+        //when
+        c.addMinutes(30);
+        //then
+        assertEquals(2, c.getHour());
+        assertEquals(0, c.getMinutes());
     }
 
     @Test
     public void add_minutes_23_59_plus_1min_gives_00_00() {
-
+        //given
+        c = new Clock(23, 59);
+        //when
+        c.addMinutes(1);
+        //then
+        assertEquals(0, c.getHour());
+        assertEquals(0, c.getMinutes());
     }
 
     @Test
     public void add_minutes_22_22_plus_300min_gives_03_22() {
-
+        //given
+        c = new Clock(22, 22);
+        //when
+        c.addMinutes(300);
+        //then
+        assertEquals(3, c.getHour());
+        assertEquals(22, c.getMinutes());
     }
     //--- addHours(), addMinutes()
 
     //--- addClock
-    @Ignore //ignoruje test
+    //@Ignore //ignoruje test
     @Test
     public void add_clock_01_30_plus_01_30_gives_03_00() {
         //given:
@@ -129,22 +181,49 @@ public class ClockTest {
 
     @Test
     public void add_clock_01_30_plus_02_30_gives_04_00() {
-
+        //given:
+        c = new Clock(1, 30);
+        Clock c2 = new Clock(2, 30);
+        //when:
+        c.addClock(c2);
+        //then:
+        assertEquals(4, c.getHour());
+        assertEquals(0, c.getMinutes());
     }
 
     @Test
     public void add_clock_01_30_plus_04_29_gives_05_59() {
-
+        //given:
+        c = new Clock(1, 30);
+        Clock c2 = new Clock(4, 29);
+        //when:
+        c.addClock(c2);
+        //then:
+        assertEquals(5, c.getHour());
+        assertEquals(59, c.getMinutes());
     }
 
     @Test
     public void add_clock_23_30_plus_02_00_gives_01_30() {
-
+        //given:
+        c = new Clock(23, 30);
+        Clock c2 = new Clock(2, 0);
+        //when:
+        c.addClock(c2);
+        //then:
+        assertEquals(1, c.getHour());
+        assertEquals(30, c.getMinutes());
     }
 
     @Test
     public void add_clock_05_00_plus_null_gives_05_00() {
-
+        //given:
+        c = new Clock(5, 0);
+        //when:
+        c.addClock(null);
+        //then:
+        assertEquals(5, c.getHour());
+        assertEquals(0, c.getMinutes());
     }
     //--- addClock
 
